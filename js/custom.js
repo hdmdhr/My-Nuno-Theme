55c85dc507a7275962321e2667e55d08360c9e73
DongMing <hdmdhr@gmail.com>
2019-06-16 23:03:41 -0600

    24. Add dark fixed image section and style it. | Some classes used for fixed img can be found in fixed.css.

--- js/custom.js
+++ js/custom.js
@@ -2,32 +2,38 @@
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
 
 $(document).ready(() => { //when document(DOM) loads completely.
-        // Transition effect for navbar
-        $(window).scroll(() => { //function is called while you scrolls
-          // checks if window is scrolled more than 300px, adds/removes solid class
-          if($(this).scrollTop() > 300)   // this refers to scroll() caller
-              $('.navbar').addClass('solid'); //add class 'solid' to 'navbar'
-          else
-              $('.navbar').removeClass('solid'); //remove class 'solid' from 'navbar'
-        });
+  function addRemoveSolid() {
+    // checks if window is scrolled more than 300px, adds/removes solid class
+    if ($(window).scrollTop() > 300)
+      $('.navbar').addClass('solid');
+    else
+      $('.navbar').removeClass('solid');
+  }
+
+  addRemoveSolid();
+
+  // Transition effect for navbar
+  $(window).scroll(() => { //function is called while you scrolls
+    addRemoveSolid();
+  });
 });
 
 /*========== CLOSE MOBILE NAV ON CLICK ==========*/
 
 $(document).ready(() => { //when document loads completely.
-    $(document).click((event) => { //click anywhere
-        let clickover = $(event.target); //get the target element where you clicked
-        let _opened = $(".navbar-collapse").hasClass("show"); //check if 'navbar-collapse' has a class 'show' (add by Bootstrap).
-        if (_opened && !clickover.hasClass("navbar-toggler")) // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
-            $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
-    });
+  $(document).click((event) => { //click anywhere
+    let clickover = $(event.target); //get the target element where you clicked
+    let _opened = $(".navbar-collapse").hasClass("show"); //check if 'navbar-collapse' has a class 'show' (add by Bootstrap).
+    if (_opened && !clickover.hasClass("navbar-toggler")) // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
+      $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
+  });
 });
 
 /*----- Turn on/off .solid background when burger menu open/close -----*/
 $(".navbar-toggler").click((event) => {
-  if ($(".navbar-collapse").hasClass("show"))  // about to close
+  if ($(".navbar-collapse").hasClass("show")) // about to close
     $('.navbar').removeClass('solid');
-  else  // about to open
+  else // about to open
     $('.navbar').addClass('solid');
 });
 
@@ -35,7 +41,7 @@ $(".navbar-toggler").click((event) => {
 
 $(document).ready(() => {
   // Add smooth scrolling to all links
-  $('a').click((event) => {//click on any link;anchor tag;
+  $('a').click((event) => { //click on any link;anchor tag;
     // event.target equals to this in function(){ this }
     // console.log(event.target);
     // Make sure this.hash has a value before overriding default behavior, e.g. href="#contact"
@@ -63,69 +69,67 @@ $(document).ready(() => {
 /*========== BOUNCING DOWN ARROW ==========*/
 //down arrow at top
 $(document).ready(() => {
-$(window).scroll(() => { //browser scroll
+  $(window).scroll(() => { //browser scroll
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); //set opacity css from 1 to -(negative) infinity of element with class 'arrow'
-  //250 pixels is disappear threshold
+    //250 pixels is disappear threshold
   });
 });
 
 /*========== MEET THE TEAM ==========*/
 //theCarousel
-$(document).ready(function(){ //when document is ready
+$(document).ready(function() { //when document is ready
   $("#team-slider").owlCarousel({ //owlCarousel settings
-        items:3, //by default there are 3 slides display.
-        autoplay:true, //the slides will change automatically.
-        smartSpeed:700, //speed of changing wil be 700
-        loop:true, //infinite loop; after the last slide, first slide starts
-        autoplayHoverPause:true, //when you put mouse over Carousel, slide changing will stop
-        responsive : { //responsiveness as screen size changes
-            // min-width: 0px
-            0 : {
-                items: 1 //on devices with width 0 to 579px show 1 slide
-            },
-            // min-width: 579px
-            576 : {
-                items: 2 //on devices with width 579px to 768px show show 2 slides
-            },
-            // min-width: 768px
-            768 : {
-                items: 3 //on devices with width 768px and above show 3 slides
-            }
-        }
-  }
-  );
+    items: 3, //by default there are 3 slides display.
+    autoplay: true, //the slides will change automatically.
+    smartSpeed: 700, //speed of changing wil be 700
+    loop: true, //infinite loop; after the last slide, first slide starts
+    autoplayHoverPause: true, //when you put mouse over Carousel, slide changing will stop
+    responsive: { //responsiveness as screen size changes
+      // min-width: 0px
+      0: {
+        items: 1 //on devices with width 0 to 579px show 1 slide
+      },
+      // min-width: 579px
+      576: {
+        items: 2 //on devices with width 579px to 768px show show 2 slides
+      },
+      // min-width: 768px
+      768: {
+        items: 3 //on devices with width 768px and above show 3 slides
+      }
+    }
+  });
 });
 
 /*========== SKILLS COUNTER ==========*/
 
 $(document).ready(function() { //when document is ready
-        $('.counter').counterUp({
-            delay: 10, //delay in milliseconds per count up
-            time: 1800 //total time taken by the animation
-        });
-    });
+  $('.counter').counterUp({
+    delay: 10, //delay in milliseconds per count up
+    time: 1800 //total time taken by the animation
+  });
+});
 
 /*========== CLIENTS CAROUSEL ==========*/
 
-$(document).ready(function(){ //when document is ready
+$(document).ready(function() { //when document is ready
   $("#clients-slider").owlCarousel({ //owlCarousel settings
-        items:2, //by default there are 2 slides display.
-        autoplay:true, //the slides will change automatically.
-        smartSpeed:1700, //speed of changing wil be 700
-        loop:true, //infinite loop; after the last slide, first slide starts
-        autoplayHoverPause:true, //when you put mouse over Carousel, slide changing will stop
-        responsive : { //responsiveness as screen size changes
-            // min-width: 0px
-            0 : {
-                items: 1 //on devices with width 0 to 768px show 1 slide
-            },
-            // min-width: 768px
-            768 : {
-                items: 2 //on devices with width 768px and above show show 2 slides
-            },
-        }
-  }
-  );
+    items: 2, //by default there are 2 slides display.
+    autoplay: true, //the slides will change automatically.
+    smartSpeed: 1700, //speed of changing wil be 700
+    loop: true, //infinite loop; after the last slide, first slide starts
+    autoplayHoverPause: true, //when you put mouse over Carousel, slide changing will stop
+    responsive: { //responsiveness as screen size changes
+      // min-width: 0px
+      0: {
+        items: 1 //on devices with width 0 to 768px show 1 slide
+      },
+      // min-width: 768px
+      768: {
+        items: 2 //on devices with width 768px and above show show 2 slides
+      },
+    }
+  });
 });
 
 /*========== TOP SCROLL BUTTON ==========*/
